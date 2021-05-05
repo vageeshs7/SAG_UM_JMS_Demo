685764583903563366d1c60e2ec87d51bce2c477
vageesh <vageeshs7@gmail.com>
2021-05-05 20:31:43 +0530

    Initial code commit

---
+++ src/main/java/org/vag/sag/umdemo/SagumDemoApplication.java
@@ -0,0 +1,13 @@
+package org.vag.sag.umdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SagumDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SagumDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/vag/sag/umdemo/SagumDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.vag.sag.umdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagumDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
